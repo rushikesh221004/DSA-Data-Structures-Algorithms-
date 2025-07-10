149c70da9c7141e70e5f72dcad3e0e08c446ac02
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-10 21:15:18 +0530

    feat: Add Counting Sort implementation with ArrayList

--- Sorting/CountingSort.java
+++ Sorting/CountingSort.java
@@ -25,6 +25,34 @@ public class CountingSort {
         }
     }
 
+    /* 
+    public static void countingSort(ArrayList<Integer> nums) {
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.size(); i++) {
+            largest = Math.max(largest, nums.get(i));
+        }
+
+        ArrayList<Integer> count = new ArrayList<>();
+        for(int i = 0; i <= largest; i++) {
+            count.add(0);
+        }
+        for(int i = 0; i < nums.size(); i++) {
+            count.set(nums.get(i), count.get(nums.get(i)) + 1);
+        }
+
+        int j = 0;
+        for(int i = 0; i < count.size(); i++) {
+            while (count.get(i) > 0) {
+                nums.set(j, i);
+                j++;
+                //count[i]--;
+                count.set(i,  count.get(i) - 1);
+            }
+        }
+
+    }
+        */
+
     public static void main(String[] args) {
         int nums[] = {1, 4, 1, 3, 2, 4, 3, 7};
         countingSort(nums);
