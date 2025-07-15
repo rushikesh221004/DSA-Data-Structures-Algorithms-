9fa2cce76d4da92c5106ac2a37a9efcc2f5927e2
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-15 23:16:58 +0530

    Initialize DiagonalMatrix.java with method stub

---
+++ 2D Arrays/DiagonalMatrix.java
@@ -0,0 +1,11 @@
+public class DiagonalMatrix {
+    public static void printDiagonalMatrix(int[][] matrix) {}
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        printDiagonalMatrix(matrix);
+    }
+}
