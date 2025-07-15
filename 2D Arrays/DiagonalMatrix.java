1c6b9953403bd784588a64db33a54cc092188fba
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-15 23:34:23 +0530

    Print top-left to bottom-right diagonal elements

--- 2D Arrays/DiagonalMatrix.java
+++ 2D Arrays/DiagonalMatrix.java
@@ -1,10 +1,21 @@
 public class DiagonalMatrix {
-    public static void printDiagonalMatrix(int[][] matrix) {}
+    public static void printDiagonalMatrix(int[][] matrix) {
+        if(matrix.length == 0) return;
+        int startCol = 0;
+        int endCol = matrix[0].length-1;
+        int startRow = 0;
+        int endRow = matrix.length-1;
+        while(startRow <= endRow && startCol <= endCol) {
+            System.out.print(matrix[startRow][startCol] + " ");
+            startRow++;
+            startCol++;
+        }
+    }
     public static void main(String[] args) {
         int[][] matrix = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+            {1, 2},
+            {3, 4},
+            {5, 6}
         };
         printDiagonalMatrix(matrix);
     }
