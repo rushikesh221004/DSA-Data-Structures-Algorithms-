098156af503d35f20d199c4b8cb7c34a7a7a0412
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-16 21:28:32 +0530

    Initialize TransposeMatrix.java with method stub

---
+++ 2D Arrays/TransposeMatrix.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class TransposeMatrix {
+    public static void printTransposeMatrix(int[][] matrix) {}
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        printTransposeMatrix(matrix);
+    }
+}
