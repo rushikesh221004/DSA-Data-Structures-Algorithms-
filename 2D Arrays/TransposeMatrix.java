d345fadbfc5932fb34b73892d97d5976b6e6d816
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-17 19:47:55 +0530

    Implement in-place transpose for square matrix

--- 2D Arrays/TransposeMatrix.java
+++ 2D Arrays/TransposeMatrix.java
@@ -1,7 +1,24 @@
-import java.util.Arrays;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class TransposeMatrix {
-    public static void printTransposeMatrix(int[][] matrix) {}
+    public static void printTransposeMatrix(int[][] matrix) {
+        if (matrix.length == 0)
+            return;
+
+        int n = matrix.length;
+
+        // In-place transpose (only for square matrix)
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Swap matrix[i][j] with matrix[j][i]
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+    }
 
     public static void main(String[] args) {
         int[][] matrix = {
