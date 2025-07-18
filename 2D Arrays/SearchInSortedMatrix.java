2c8cfb75961473acd64ccbefaf4826a9176cb8d1
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-18 21:17:10 +0530

    Implement brute-force search in sorted 2D matrix (O(n²))

--- 2D Arrays/SearchInSortedMatrix.java
+++ 2D Arrays/SearchInSortedMatrix.java
@@ -1,5 +1,15 @@
 public class SearchInSortedMatrix {
-    public static boolean searchMatrixBrute(int[][] matrix, int target) {return false;}
+    public static boolean searchMatrixBrute(int[][] matrix, int target) {
+        int n = matrix.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                if(matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         int[][] matrix = {
@@ -7,7 +17,8 @@ public class SearchInSortedMatrix {
                 { 7, 9, 11 },
                 { 13, 15, 17 }
         };
-        int target = 9;
-        searchMatrixBrute(matrix, target);
+        int target = 13;
+        boolean bruteForce = searchMatrixBrute(matrix, target);
+        System.out.print(bruteForce);
     }
 }
