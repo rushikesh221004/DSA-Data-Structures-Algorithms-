a03f854a150bc207b593e3dcd88ebf9cd75b51be
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-18 21:12:50 +0530

    Setup boilerplate for search in a sorted 2D matrix using brute force approach

---
+++ 2D Arrays/SearchInSortedMatrix.java
@@ -0,0 +1,13 @@
+public class SearchInSortedMatrix {
+    public static boolean searchMatrixBrute(int[][] matrix, int target) {return false;}
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                { 1, 3, 5 },
+                { 7, 9, 11 },
+                { 13, 15, 17 }
+        };
+        int target = 9;
+        searchMatrixBrute(matrix, target);
+    }
+}
