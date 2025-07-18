753cb23992d20b6e1756e30f05e1c0d08496ba3b
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-18 19:29:57 +0530

    Setup boilerplate for Rotate Matrix 90° clockwise (in-place)

---
+++ 2D Arrays/RotateMatrixInPlace.java
@@ -0,0 +1,11 @@
+public class RotateMatrixInPlace {
+    public static void RotateMatrix(int[][] matrix) {}
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        RotateMatrix(matrix);
+    }
+}
