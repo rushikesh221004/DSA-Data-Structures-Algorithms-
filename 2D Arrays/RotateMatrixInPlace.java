fd4856b96c49dd2028cfaf740f5705e79d6a40f4
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-18 21:02:39 +0530

    Implement in-place 90° clockwise matrix rotation using transpose and row reversal

--- 2D Arrays/RotateMatrixInPlace.java
+++ 2D Arrays/RotateMatrixInPlace.java
@@ -1,10 +1,36 @@
 public class RotateMatrixInPlace {
-    public static void RotateMatrix(int[][] matrix) {}
+    public static void RotateMatrix(int[][] matrix) {
+        if (matrix.length == 0)
+            return;
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int start = 0, end = n - 1;
+            while (start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+
+                start++;
+                end--;
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
         int matrix[][] = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
         };
         RotateMatrix(matrix);
     }
