6e5eef707e6bec0982d4dbfed9f04953d59c3ff0
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-18 21:30:41 +0530

    Add optimized binary search approach for sorted 2D matrix

--- 2D Arrays/SearchInSortedMatrix.java
+++ 2D Arrays/SearchInSortedMatrix.java
@@ -1,9 +1,9 @@
 public class SearchInSortedMatrix {
     public static boolean searchMatrixBrute(int[][] matrix, int target) {
         int n = matrix.length;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < matrix[i].length; j++) {
-                if(matrix[i][j] == target) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == target) {
                     return true;
                 }
             }
@@ -12,6 +12,19 @@ public class SearchInSortedMatrix {
     }
 
     public static boolean searchMatrixBinary(int[][] matrix, int target) {
+        for (int i = 0; i < matrix.length; i++) {
+            int start = 0, end = matrix[i].length - 1;
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (matrix[i][mid] == target) {
+                    return true;
+                } else if (matrix[i][mid] > target) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
         return false;
     }
 
@@ -21,8 +34,9 @@ public class SearchInSortedMatrix {
                 { 7, 9, 11 },
                 { 13, 15, 17 }
         };
-        int target = 13;
+        int target = 3;
         boolean bruteForce = searchMatrixBrute(matrix, target);
-        System.out.print(bruteForce);
+        boolean binarySearch = searchMatrixBinary(matrix, target);
+        System.out.print(binarySearch);
     }
 }
