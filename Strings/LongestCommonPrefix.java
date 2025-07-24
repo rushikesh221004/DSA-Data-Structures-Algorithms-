4bd1e38612e44391c6f57368b007b35a8160357c
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-07-24 20:59:07 +0530

    trying to small bug fixes

--- Strings/LongestCommonPrefix.java
+++ Strings/LongestCommonPrefix.java
@@ -1,22 +1,26 @@
 public class LongestCommonPrefix {
     public static void longestCommonPrefix(String[] str) {
-        String longest = "";
-        for(int i = 1; i < str.length-1; i++) {
-            longest = "";
-            for(int j = 0; j < str[i].length(); j++) {
-                if(longest.charAt(j) == str[i].charAt(j)) {
-                    longest += 
+        int minLength = Integer.MAX_VALUE;
+        for(int i = 0; i < str.length; i++) {
+            minLength = Math.min(minLength, str[i].length());
+        }
+        
+        StringBuilder longestPrefix = new StringBuilder("");
+        for(int i = 0; i < str.length-1; i++) {
+            longestPrefix.setLength(0);
+            for(int j = 0; j < minLength; j++) {
+                if(str[0].charAt(j) == str[i+1].charAt(j)) {
+                    longestPrefix.append(str[0].charAt(j));
+                } else {
+                    break;
                 }
             }
         }
+        System.out.print(longestPrefix);
     }
     public static void main(String[] args) {
-        String[] str = {"flower", "flow", "flight"};
-        for(int i = 0; i < str.length; i++) {
-            for(int j = 0; j < str[i].length(); j++) {
-                System.out.print(str[i].charAt(j) + " ");
-            }
-            System.out.println();
-        }
+        String[] str = {"flair", "flow", "flight"};
+
+        longestCommonPrefix(str);
     }
 }
